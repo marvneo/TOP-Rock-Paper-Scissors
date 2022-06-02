aac4fbd9beffe732f543420ca2fea84fac81e3cc
Marvin Neoh <neoh@ualberta.ca>
2022-06-01 22:13:54 -0400

    allow game play with R,P,S buttons

--- myscripts.js
+++ myscripts.js
@@ -1,25 +1,36 @@
+score = 0;
+compScore = 0;
 
+     
+const click = document.querySelector('#buttons');
+
+click.addEventListener('click', function(e) {
+    playRound(e.srcElement.id);
+    })    
+        
+// Outputs the computer's choice
 function computerPlay() {
 
     roll = Math.random()
 
     if (roll <= .32) {
-        return 'Rock'
+        return 'rock'
     } else if (roll <= .65) {
-        return 'Paper'
+        return 'paper'
     } else {
-        return 'Scissors'
+        return 'scissors'
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+// Determine who wins 
+function whoWins(playerSelection, computerSelection) {
 
     win = false
 
     var winningCombo = {
-        'Rock': 'Scissors',
-        'Scissors': 'Paper',
-        'Paper': 'Rock'
+        'rock': 'scissors',
+        'scissors': 'paper',
+        'paper': 'rock'
     }
 
     if (winningCombo[playerSelection] === computerSelection) {
@@ -29,36 +40,32 @@ function playRound(playerSelection, computerSelection) {
     return win
 }
 
-function game() {
 
-    score = 0;
-    compScore = 0;
-    
+// Plays one round
+function playRound(playerSelection) {
 
-    //while (score < 5 && compScore < 5)  {
 
         computerSelection = computerPlay()
-        playerSelection = prompt('Please enter Rock, Paper or Scissors')
-        win = playRound(playerSelection, computerSelection)
+        
+        if (playerSelection === computerSelection) {
+            console.log('It\'\s a draw!')
+        } else {
+            win = whoWins(playerSelection, computerSelection)
+            if (win) {
+                console.log('You win!')
+                score ++;
+            } else {
+                console.log('You lost!')
+                compScore ++;
+            }
+        }
+
+        
         console.log('Computer chose ' + computerSelection, '. You chose ' + playerSelection)
 
-        if (win) {
-            console.log('You win!')
-            score ++;
-        } else {
-            console.log('You lost!')
-            compScore ++;
-        }
+        
         console.log('Your score is '+score+'. The computer score is ' + compScore)
-  //  }
-    
-  /*  if (score > compScore) {
-        console.log('You won the game!')
-    } else {
-        console.log('You lost the game...')
     }
-    */
 
-}
 
-game()
+
